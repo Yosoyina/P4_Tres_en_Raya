f064740acfd37964fb49b37bd42c8903c2552457
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-12-03 17:13:21 +0100

    Apartado de la Moneda
    En esta parte decimos de los numeros aleatorios que es la moneda de la cara y la cruz ademas de añadir las variables arriba y cerar ek random

--- src/Ejercicios/P4_Tres_en_Raya.java
+++ src/Ejercicios/P4_Tres_en_Raya.java
@@ -1,11 +1,13 @@
 package Ejercicios;
 import java.util.Scanner;
+import java.util.Random;
 public class P4_Tres_en_Raya {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner teclado = new Scanner(System.in);
+		Random numAl = new Random ();
 		
 		// Apartado de los Jugadores
 		
@@ -17,6 +19,12 @@ public class P4_Tres_en_Raya {
 		int contJ1 = 0;
 		int contJ2 = 0;
 		
+		// Apartado de la moneda
+		
+		int moneda = 0;
+		
+		moneda = numAl.nextInt(2)+1;
+			
 		// Apartado del Tablero del Tres en raya
 		
 		String [][] Tabla = {{"-", "-", "-"},
@@ -46,6 +54,31 @@ public class P4_Tres_en_Raya {
 		
 		Jugador2 = teclado.next(); // El jugador 2 introuducce su nombre
 		
+		// Aparatado de la Condicion ( Cara ) que es igual a 1
+		
+		if(moneda == 1) {
+			
+			System.out.println(" ______________________");
+			System.out.println("|                      |");
+			System.out.println("|    Ha salido Cara    |");
+			System.out.println("|______________________|");
+			
+		}
+		
+		// Aparatado de la Condicion ( Cruz ) que es igual a 2
+		
+		else if (moneda == 2) {
+			
+			System.out.println(" ______________________");
+			System.out.println("|                      |");
+			System.out.println("|    Ha salido Cruz    |");
+			System.out.println("|______________________|");
+			
+		}
+		
+		
+		
+	
 		
 		
 		
