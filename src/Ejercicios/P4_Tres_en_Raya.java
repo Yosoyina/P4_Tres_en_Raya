21772b046762f3ab51a258f6ab928463f76d11f7
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-12-03 16:28:39 +0100

    Creacion de los Jugadores y Introduccir el nombre
    En este apartado tenemos los dos jugadores donde tendran que introduccir su nombre de cada uno de los jugadores y como vemos arriba ha dos contadores de los jugadores para saber las rondas que han ganado

--- src/Ejercicios/P4_Tres_en_Raya.java
+++ src/Ejercicios/P4_Tres_en_Raya.java
@@ -1,10 +1,47 @@
 package Ejercicios;
-
+import java.util.Scanner;
 public class P4_Tres_en_Raya {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner(System.in);
+		
+		// Apartado de los Jugadores
+		
+		String Jugador1;
+		String Jugador2;
+		
+		// Apartado de los contadores de los jugadores 
+		
+		int contJ1 = 0;
+		int contJ2 = 0;
+		
+		
+		System.out.println(" ______________________________________________________");
+		System.out.println("|                                                      |");
+		System.out.println("|         Bienvenido al Juego de Tres en Raya          |");
+		System.out.println("|______________________________________________________|");
+		
+		
+		System.out.println(" ______________________________________________________");
+		System.out.println("|                                                      |");
+		System.out.println("|          Introducce el nombre al Jugador 1           |");
+		System.out.println("|______________________________________________________|");
+		
+		Jugador1 = teclado.next();
+		
+		
+		System.out.println(" ______________________________________________________");
+		System.out.println("|                                                      |");
+		System.out.println("|          Introducce el nombre al Jugador 2           |");
+		System.out.println("|______________________________________________________|");
+		
+		Jugador2 = teclado.next();
+		
+		
+		
+		
 	}
 
 }
