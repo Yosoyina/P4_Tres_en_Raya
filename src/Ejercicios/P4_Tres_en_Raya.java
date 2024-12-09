6d203ac72e6ebad5ee90136d5494ebc8a506a855
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-12-09 15:57:33 +0100

    Espacios y Recorrido de la Tabla
    En esta parte hacemos el recorrido de la tabla del tres en raya y añadimos espacios en algunas partes

--- src/Ejercicios/P4_Tres_en_Raya.java
+++ src/Ejercicios/P4_Tres_en_Raya.java
@@ -38,6 +38,7 @@ public class P4_Tres_en_Raya {
 		System.out.println("|         Bienvenido al Juego de Tres en Raya          |");
 		System.out.println("|______________________________________________________|");
 		
+		System.out.println("");
 		
 		System.out.println(" ______________________________________________________");
 		System.out.println("|                                                      |");
@@ -46,6 +47,7 @@ public class P4_Tres_en_Raya {
 		
 		Jugador1 = teclado.next(); // El jugador 1 introuducce su nombre
 		
+		System.out.println("");
 		
 		System.out.println(" ______________________________________________________");
 		System.out.println("|                                                      |");
@@ -77,6 +79,28 @@ public class P4_Tres_en_Raya {
 			System.out.println("|______________________|");
 			
 		}
+		
+		System.out.println("");
+		
+		// Parte de las rondas 
+		
+		
+		System.out.println(" ______________________________________________________");
+		System.out.println("|                                                      |");
+		System.out.println("|          Muestra de Tabla del tres en Raya           |");
+		System.out.println("|______________________________________________________|");
+		
+		System.out.println("");
+		
+		for (int i = 0; i < Tabla.length; i++) {
+			for (int j = 0; j < Tabla[i].length; j++) {
+				
+				System.out.print(Tabla[i][j] + " ");
+				
+			}
+			System.out.println("");
+		}
+		
 		
 		
 		
